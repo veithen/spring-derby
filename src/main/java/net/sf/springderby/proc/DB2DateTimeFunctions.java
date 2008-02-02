1dc462249aea2f5e7877e596c47bfd495322d495
veithen <veithen@1caec158-f444-0410-b9bd-d67fc352de81>
2008-02-02 19:34:37 +0000

    Completed DB2 date/time function emulation.
    git-svn-id: https://svn.code.sf.net/p/spring-derby/code/trunk@18 1caec158-f444-0410-b9bd-d67fc352de81

--- src/main/java/net/sf/springderby/proc/DB2DateTimeFunctions.java
+++ src/main/java/net/sf/springderby/proc/DB2DateTimeFunctions.java
@@ -17,30 +17,107 @@ import java.sql.Date;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+/**
+ * Emulation of DB2 date and time functions.
+ * <p>
+ * This class contains a set of static methods that emulate various DB2 date and
+ * time functions.
+ * 
+ * @author Andreas Veithen
+ */
 public class DB2DateTimeFunctions {
-	// DAYOFMONTH
-	// The DAYOFMONTH function returns the day part of a value. The function is similar to the DAY function, except DAYOFMONTH does not support a date or timestamp duration as an argument.
-	// if the argument is null, the result is the null value.
+	private DB2DateTimeFunctions() {}
 	
-	// DAYOFWEEK
-	// The DAYOFWEEK function returns an integer in the range of 1 to 7 that represents the day of the week where 1 is Sunday and 7 is Saturday. The DAYOFWEEK function is similar to the DAYOFWEEK_ISO function.
-	// if the argument is null, the result is the null value.
+	/**
+	 * Emulation of the DB2 <tt>DAYOFWEEK</tt> function.
+	 * This function is defined as follows:
+	 * <blockquote>
+	 * The DAYOFWEEK function returns an integer in the range of 1 to 7 that represents the
+	 * day of the week where 1 is Sunday and 7 is Saturday.
+	 * [...] if the argument is null, the result is the null value.
+	 * </blockquote>
+	 * @param date the date value
+	 * @return the day of the week or <code>null</code> if <code>date</code> is <code>null</code>
+	 */
+	public static Integer dayOfWeek(Date date) {
+		if (date == null) {
+			return null;
+		} else {
+			GregorianCalendar calendar = new GregorianCalendar();
+			calendar.setTime(date);
+			return new Integer(calendar.get(Calendar.DAY_OF_WEEK));
+		}
+	}
 	
-	// DAYOFWEEK_ISO
-	// The DAYOFWEEK_ISO function returns an integer in the range of 1 to 7 that represents the day of the week, where 1 is Monday and 7 is Sunday. The DAYOFWEEK_ISO function is similar to the DAYOFWEEK function.
-	// if the argument is null, the result is the null value.
+	/**
+	 * Emulation of the DB2 <tt>DAYOFWEEK_ISO</tt> function.
+	 * This function is defined as follows:
+	 * <blockquote>
+	 * The DAYOFWEEK_ISO function returns an integer in the range of 1 to 7 that
+	 * represents the day of the week, where 1 is Monday and 7 is Sunday.
+	 * [...] if the argument is null, the result is the null value.
+	 * @param date the date value
+	 * @return the day of the week or <code>null</code> if <code>date</code> is <code>null</code>
+	 */
+	public static Integer dayOfWeekIso(Date date) {
+		if (date == null) {
+			return null;
+		} else {
+			GregorianCalendar calendar = new GregorianCalendar();
+			calendar.setTime(date);
+			int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+			return new Integer(dayOfWeek == 1 ? 7 : dayOfWeek-1);
+		}
+	}
 	
-	// DAYOFYEAR
-	// The DAYOFYEAR function returns an integer in the range of 1 to 366 that represents the day of the year where 1 is January 1.
-	// if the argument is null, the result is the null value.
+	/**
+	 * Emulation of the DB2 <tt>DAYOFYEAR</tt> function.
+	 * This function is defined as follows:
+	 * <blockquote>
+	 * The DAYOFYEAR function returns an integer in the range of 1 to 366 that
+	 * represents the day of the year where 1 is January 1.
+	 * [...] if the argument is null, the result is the null value.
+	 * </blockquote>
+	 * @param date the date value
+	 * @return the day of the year or <code>null</code> if <code>date</code> is <code>null</code>
+	 */
+	public static Integer dayOfYear(Date date) {
+		if (date == null) {
+			return null;
+		} else {
+			GregorianCalendar calendar = new GregorianCalendar();
+			calendar.setTime(date);
+			return new Integer(calendar.get(Calendar.DAY_OF_YEAR));
+		}
+	}
 	
-	// LAST_DAY
-	// The LAST_DAY scalar function returns a date that represents the last day of the month of the date argument.
-	// if the value of date-expression is null, the result is the null value.
+	/**
+	 * Emulation of the DB2 <tt>LAST_DAY</tt> function.
+	 * This function is defined as follows:
+	 * <blockquote>
+	 * The LAST_DAY scalar function returns a date that represents the last day
+	 * of the month of the date argument.
+	 * [...] if the value of date-expression is null, the result is the null value.
+	 * </blockquote>
+	 * @param date the date value
+	 * @return the last day of the month or <code>null</code> if <code>date</code> is <code>null</code>
+	 */
+	public static Date lastDay(Date date) {
+		if (date == null) {
+			return null;
+		} else {
+			GregorianCalendar calendar = new GregorianCalendar();
+			calendar.setTime(date);
+			calendar.set(Calendar.DAY_OF_MONTH, 1);
+			calendar.add(Calendar.MONTH, 1);
+			calendar.add(Calendar.DAY_OF_MONTH, -1);
+			return new Date(calendar.getTimeInMillis());
+		}
+	}
 	
 	/**
 	 * Emulation of the DB2 <tt>WEEK</tt> function.
-	 * The function is defined as follows:
+	 * This function is defined as follows:
 	 * <blockquote>
 	 * The WEEK function returns an integer in the range of 1 to 54 that represents the week
 	 * of the year. The week starts with Sunday, and January 1 is always in the first week.
@@ -61,7 +138,30 @@ public class DB2DateTimeFunctions {
 		}
 	}
 	
-	// WEEK_ISO
-	// The WEEK_ISO function returns an integer in the range of 1 to 53 that represents the week of the year. The week starts with Monday and includes 7 days. Week 1 is the first week of the year to contain a Thursday, which is equivalent to the first week containing January 4. Thus, it is possible to have up to 3 days at the beginning of the year appear as the last week of the previous year, or to have up to 3 days at the end of a year appear as the first week of the next year.
-	// if the argument is null, the result is the null value.
+	/**
+	 * Emulation of the DB2 <tt>WEEK_ISO</tt> function.
+	 * This function is defined as follows:
+	 * <blockquote>
+	 * The WEEK_ISO function returns an integer in the range of 1 to 53 that represents the week
+	 * of the year. The week starts with Monday and includes 7 days. Week 1 is the first week
+	 * of the year to contain a Thursday, which is equivalent to the first week containing
+	 * January 4. Thus, it is possible to have up to 3 days at the beginning of the year appear
+	 * as the last week of the previous year, or to have up to 3 days at the end of a year
+	 * appear as the first week of the next year.
+	 * [...] if the argument is null, the result is the null value.
+	 * </blockquote>
+	 * @param date the date value
+	 * @return the week of the year or <code>null</code> if <code>date</code> was <code>null</code>
+	 */
+	public static Integer weekIso(Date date) {
+		if (date == null) {
+			return null;
+		} else {
+			GregorianCalendar calendar = new GregorianCalendar();
+			calendar.setTime(date);
+			calendar.setFirstDayOfWeek(Calendar.MONDAY);
+			calendar.setMinimalDaysInFirstWeek(4);
+			return new Integer(calendar.get(Calendar.WEEK_OF_YEAR));
+		}
+	}
 }
