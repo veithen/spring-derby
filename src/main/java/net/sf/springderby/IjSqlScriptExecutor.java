99267dcec19b6d7942875c17bead355679bbd781
veithen <veithen@1caec158-f444-0410-b9bd-d67fc352de81>
2008-02-08 20:18:11 +0000

    Handled compatibility issues.
    git-svn-id: https://svn.code.sf.net/p/spring-derby/code/trunk@43 1caec158-f444-0410-b9bd-d67fc352de81

--- src/main/java/net/sf/springderby/IjSqlScriptExecutor.java
+++ src/main/java/net/sf/springderby/IjSqlScriptExecutor.java
@@ -1,3 +1,16 @@
+/**
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
 package net.sf.springderby;
 
 import java.io.IOException;
@@ -11,6 +24,15 @@ import org.apache.derby.tools.ij;
 import org.springframework.core.io.Resource;
 import org.springframework.dao.DataAccessException;
 
+/**
+ * SQL Script executor based on {@link ij}.
+ * Note that this executor requires version 10.2.1.6 or later of <tt>derbytools.jar</tt> since it
+ * depends on the {@link ij#runScript(Connection, InputStream, String, java.io.OutputStream, String)} method,
+ * which doesn't exist in prior versions.
+ * 
+ * @author Andreas Veithen
+ * @version $Id$
+ */
 public class IjSqlScriptExecutor implements SqlScriptExecutor {
 	private final static Log log = LogFactory.getLog(IjSqlScriptExecutor.class);
 	
